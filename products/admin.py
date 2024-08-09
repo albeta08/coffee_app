251f5c86d84a985171703e69f57a3cbae4ab5ce0
albeta08 <albeta1258@gmail.com>
2024-08-09 18:50:35 -0500

    add urls and viewa

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+class ProductAdmnin(admin.ModelAdmin):
+    model = Product
+    list_display = ['name', 'price', 'available','photo']
+    search_fields = ['name']
+
+admin.site.register(Product, ProductAdmnin)
